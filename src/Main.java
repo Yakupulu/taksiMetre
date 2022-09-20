398ecf9a84de2366c66dbb30909402f7fe3eeeca
yakupulu <110382043+Yakupulu@users.noreply.github.com>
2022-09-20 23:22:29 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
 
-        double perKm=2.20,topucret,km;
+        double perKm=2.20, topucret,km;
         int basucret=10;
         Scanner input=new Scanner(System.in);
         System.out.print("Mesafeyi Km cinsinden  giriniz: ");
